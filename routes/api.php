e8b000c0b68040ca0a27e70c6bdb0b44e0c6c9f7
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2026-02-18 12:30:44 +0100

    updated

--- routes/api.php
+++ routes/api.php
@@ -71,8 +71,8 @@ Route::get('/vendor-requests', [VendorRequestController::class, 'getByEmail']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('reviews')->group(function () {
         Route::post('/', [ReviewController::class, 'store']);
-        Route::put('/{reviews}', [ReviewController::class, 'update']);
-        Route::delete('/{reviews}', [ReviewController::class, 'destroy']);
+        Route::put('/{review}', [ReviewController::class, 'update']);
+        Route::delete('/{review}', [ReviewController::class, 'destroy']);
     });
 
     // ==================== USER ORDER ROUTES ====================
